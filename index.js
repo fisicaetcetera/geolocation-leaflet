1417c070849e445d911b5b1a10197ad53854ea3f
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2020-02-15 13:08:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}))
+//blablabla
 
 const database = new Datastore('database.db');
 database.loadDatabase();
